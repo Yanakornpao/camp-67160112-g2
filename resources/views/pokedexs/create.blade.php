8e6624cd5dfc1f270e5dd42cb05fb2f94179eca1
Yanakorn Nidorn <yanakorn_pao@hotmail.com>
2026-01-09 22:37:05 +0700

    ส่งการบ้านlab4 crud

--- resources/views/pokedexs/create.blade.php
+++ resources/views/pokedexs/create.blade.php
@@ -3,11 +3,18 @@
 <input name="name" placeholder="Name">
 <input name="type" placeholder="Type">
 <input name="species" placeholder="Species">
-<input name="height" placeholder="Height">
-<input name="weight" placeholder="Weight">
-<input name="hp" placeholder="HP">
-<input name="attack" placeholder="Attack">
-<input name="defense" placeholder="Defense">
-<input name="image_url" placeholder="Image URL">
+<input type="number" name="height">
+<input type="number" name="weight">
+<input type="number" step="0.01" name="hp">
+<input type="number" step="0.01" name="attack">
+<input type="number" step="0.01" name="defense">
+<input name="image_url" id="image_url" placeholder="Image URL">
 <button type="submit">Save</button>
 </form>
+<img id="preview" width="150">
+
+<script>
+document.getElementById('image_url').addEventListener('input', function(){
+    document.getElementById('preview').src = this.value;
+});
+</script>
--- resources/views/pokedexs/index.blade.php
+++ resources/views/pokedexs/index.blade.php
@@ -1,31 +1,32 @@
-<a href="/pokedexs/create">Add Pokemon</a>
-
 <table border="1">
-    <tr>
-        <th>Name</th>
-        <th>Type</th>
-        <th>HP</th>
-        <th>Attack</th>
-        <th>Defense</th>
-        <th>Action</th>
-    </tr>
+<tr>
+    <th>Image</th>
+    <th>Name</th>
+    <th>Type</th>
+    <th>HP</th>
+    <th>Attack</th>
+    <th>Defense</th>
+    <th>Action</th>
+</tr>
 
-    @foreach($pokedexs as $p)
-    <tr>
-        <td>{{ $p->name }}</td>
-        <td>{{ $p->type }}</td>
-        <td>{{ $p->hp }}</td>
-        <td>{{ $p->attack }}</td>
-        <td>{{ $p->defense }}</td>
-        <td>
-            <a href="/pokedexs/{{ $p->id }}/edit">Edit</a>
-
-            <form action="/pokedexs/{{ $p->id }}" method="POST">
-                @csrf
-                @method('DELETE')
-                <button type="submit">Delete</button>
-            </form>
-        </td>
-    </tr>
-    @endforeach
+@foreach($pokedexs as $p)
+<tr>
+    <td>
+        <img src="{{ $p->image_url }}" width="80">
+    </td>
+    <td>{{ $p->name }}</td>
+    <td>{{ $p->type }}</td>
+    <td>{{ $p->hp }}</td>
+    <td>{{ $p->attack }}</td>
+    <td>{{ $p->defense }}</td>
+    <td>
+        <a href="/pokedexs/{{ $p->id }}/edit">Edit</a>
+        <form action="/pokedexs/{{ $p->id }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit">Delete</button>
+        </form>
+    </td>
+</tr>
+@endforeach
 </table>
